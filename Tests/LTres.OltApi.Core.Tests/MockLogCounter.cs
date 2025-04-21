383dd6614b847b8049bbbd97a899c52d16bad50e
Luciano Tres <lucianotres@outlook.com>
2025-04-20 23:18:19 -0300

    New tests for WorkProbeCache #46

---
+++ Tests/LTres.OltApi.Core.Tests/MockLogCounter.cs
@@ -0,0 +1,23 @@
+﻿using LTres.OltApi.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTres.OltApi.Core.Tests;
+
+public class MockLogCounter : ILogCounter
+{
+    public void AddCount(string category, int quantity, TimeSpan? timeDone = null) { }
+
+    public void AddError(Guid id, string category, TimeSpan? timeDone = null, Exception? error = null) { }
+
+    public void AddSuccess(Guid id, string category, TimeSpan? timeDone = null) { }
+
+    public string? PrintOutAndReset() => null;
+
+    public void RegisterHookOnPrintResetAction<T>(Action<ILogCounter> hookAction) where T : class { }
+    
+}
+
