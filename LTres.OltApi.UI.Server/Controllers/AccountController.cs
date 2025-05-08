142a8ad34feb462b4c193959d62d441f4c0c35a2
Luciano Tres <lucianotres@outlook.com>
2025-05-08 15:11:38 -0300

    Moved UI project location and included an authentication based on server to client openId.
    This new approach make possible to use many providers to authentic.
    Use LTres.OltApi.UI.Server to host the backend for WASM client.

---
+++ LTres.OltApi.UI.Server/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace LTres.OltApi.UI.Server;
+
+
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    [HttpGet("Login/{scheme}")]
+    public IActionResult Login(string scheme, string returnUrl)
+    {
+        var redirectUrl = !string.IsNullOrEmpty(returnUrl) ? returnUrl : "/";
+        return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, scheme.ToLower());
+    }
+
+    [Authorize]
+    [HttpGet("Logout")]
+    public IActionResult Logout()
+    {
+        var authProperties = new AuthenticationProperties
+        {
+            RedirectUri = "/"
+        };
+
+        var authScheme = User.FindFirstValue("authentication_scheme");
+        if (authScheme != null && authScheme != "google")
+            return SignOut(authProperties, CookieAuthenticationDefaults.AuthenticationScheme, authScheme);
+
+        return SignOut(authProperties, CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+}
---
+++ LTres.OltApi.UI.Server/Pages/Error.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace LTres.OltApi.UI.Server.Pages
+{
+    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //[IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; } = string.Empty;
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
+}
