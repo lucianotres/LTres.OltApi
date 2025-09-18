0dfabbc23f208a657dcf3f1d23ef2eecfaf8617b
Luciano Tres <lucianotres@outlook.com>
2025-09-18 15:34:13 -0300

    Tests reviewed

--- Tests/LTres.Olt.Api.Core.Tests/Workers/WorkPingActionTests.cs
+++ Tests/LTres.Olt.Api.Core.Tests/Workers/WorkPingActionTests.cs
@@ -1,21 +1,22 @@
 using LTres.Olt.Api.Common.Models;
 using LTres.Olt.Api.Core.Workers;
 using System.Net;
-using System.Net.NetworkInformation;
-using System.Threading;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace LTres.Olt.Api.Core.Tests.Workers;
 
 public class WorkPingActionTests
 {
+    private readonly WorkPingAction workPingAction = new();
+
     [Fact]
     public async Task Execute_ShouldReturnSuccessfulResponse_WhenPingSucceeds()
     {
         var id = Guid.NewGuid();
-        var workPingAction = new WorkPingAction();
-        var probeInfo = new WorkProbeInfo { Id = id, Host = new IPEndPoint(IPAddress.Loopback, 0) };
+        var probeInfo = new WorkProbeInfo
+        {
+            Id = id,
+            Host = new IPEndPoint(IPAddress.Loopback, 0)
+        };
         var cancellationToken = CancellationToken.None;
 
         var response = await workPingAction.Execute(probeInfo, cancellationToken);
@@ -32,12 +33,15 @@ public class WorkPingActionTests
     public async Task Execute_ShouldModifyInitialResponse_WhenInitialResponseIsProvided()
     {
         var id = Guid.NewGuid();
-        var mockPingAction = new WorkPingAction();
-        var probeInfo = new WorkProbeInfo { Id = id, Host = new IPEndPoint(IPAddress.Loopback, 0) };
+        var probeInfo = new WorkProbeInfo
+        {
+            Id = id,
+            Host = new IPEndPoint(IPAddress.Loopback, 0)
+        };
         var initialResponse = new WorkProbeResponse { Id = id, ValueInt = 0, Success = false };
         var cancellationToken = CancellationToken.None;
 
-        var response = await mockPingAction.Execute(probeInfo, cancellationToken, initialResponse);
+        var response = await workPingAction.Execute(probeInfo, cancellationToken, initialResponse);
 
         Assert.NotNull(response);
         Assert.Equal(id, response.Id);
@@ -51,12 +55,15 @@ public class WorkPingActionTests
     public async Task Execute_ShouldRespectCancellationToken()
     {
         var id = Guid.NewGuid();
-        var mockPingAction = new WorkPingAction();
-        var probeInfo = new WorkProbeInfo { Id = id, Host = new IPEndPoint(IPAddress.Loopback, 0) };
+        var probeInfo = new WorkProbeInfo
+        {
+            Id = id,
+            Host = new IPEndPoint(IPAddress.Loopback, 0)
+        };
         using var cts = new CancellationTokenSource();
         cts.Cancel();
 
-        var response = await mockPingAction.Execute(probeInfo, cts.Token);
+        var response = await workPingAction.Execute(probeInfo, cts.Token);
 
         Assert.NotNull(response);
         Assert.Equal(id, response.Id);
